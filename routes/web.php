56279562521c528745e6867795b31cf245ae665d
MEHEDI HASSAN JIBON <mjjibon114@gmail.com>
2025-08-30 16:17:27 +0600

    Project Install database connect Frontend Backend controller crate product structure create success with view return by the controller

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backnd\BackendController;
+use App\Http\Controllers\Frontend\FrontendController;
+
+
+
+
+
+Route::controller(FrontendController::class)->group(function () {
+    Route::get('/', 'index')->name('home.index');
+});
+
+Route::controller(BackendController::class)->group(function () {
+    Route::get('/admin/dashboard', 'index')->name('admin.dashboard');
+});
