820da51a962c0ef9bfab9ee4183117c7b277ba74
MEHEDI HASSAN JIBON <mjjibon114@gmail.com>
2025-08-30 16:53:52 +0600

    refactor(frontend): update homepage layout
    - Changed homepage view
    - Updated header and navbar
    - Improved layout structure
    - Added sign-up modal
    - Refactored asset paths

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -9,6 +9,6 @@ class FrontendController extends Controller
 {
     public function index()
     {
-        return view('frontend.layouts.frontend-master');
+        return view('frontend.pages.index');
     }
 }
